3172cf0abbeb02bb884191efb3ac0ad637553d32
aminadione <dionea0314@students.bowiestate.edu>
2023-09-27 13:33:15 -0400

    lab2files

---
+++ Lab2.py
@@ -0,0 +1,3 @@
+print('Aminata Dione')
+print('Business Information Sytems')
+print('I love to read and dance to music')
--- Lab2partbb.py
+++ Lab2partbb.py
@@ -11,8 +11,8 @@ number1=13
 number2=12
 number3=11
 number4=10
-add=number1+number2+number3+number4
-mult=number1*number2*number3*number4
-subtract=number4-number3-number2-number1
-division=(number4/number3)
+add=number1+number2
+mult=number3*number4
+subtract=number1-number2
+division=(number1/number2)
 print (add,mult,subtract,division)
