036e43efc8b95d1946b4a4e8078cc9533b492b8f
aminadione <dionea0314@students.bowiestate.edu>
2023-10-16 13:55:56 -0400

    Lab3

---
+++ Lab3.py
@@ -0,0 +1,33 @@
+number1=input('Enter number 1')
+number2=input('Enter number 2')
+sum= int(number1) + int (number2)
+print(sum)
+
+multiplication = int(number1)* int(number2)
+print(multiplication)
+print(type(multiplication))
+
+
+number1=input('Enter number 1')
+number2=input('Enter number 2')
+number3=input('Enter number3')
+sum=int(number1)+int(number2)+int(number3)
+difference=int(number1)-int(number2)-int(number3)
+multiplication=int(number1)*int(number2)*int(number3)
+division=(int(number1)/int(number2))/int(number3)
+print(sum, division, multiplication, difference)
+import math
+print(math.pow(9, 3))
+print(math.factorial(2))
+print (math.atan(0.46))
+print(math.floor(0.5))
+print(math.fsum([3, 7, 3, 4, 6]))
+print(math.gamma(5))
+print(math.log2(4))
+print (math.perm(7, 9))
+print (math.remainder(10, 4))
+print(math.tanh(3))
+print (math.sqrt(10))
+
+
+
